cc94593d7c265c1e6f11fb282380e5ab86b8e807
Johannes Olofsson <johannesolofsson@hotmail.com>
2017-12-07 21:44:34 +0100

    Integration test project has been added

---
+++ IntegrationTests/TodoController/Get.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApi.Models;
+using Xunit;
+
+namespace IntegrationTests.TodoController
+{
+    public class Get : TodoControllerTestBase
+    {
+        private readonly HttpClient _client;
+
+        public Get()
+        {
+            _client = base.GetClient();
+        }
+
+        [Fact]
+        public async Task ReturnsListOfTodo()
+        {
+            var response = await _client.GetAsync($"/api/todo");
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<IEnumerable<Todo>>(stringResponse).ToList();
+
+            Assert.Equal(3, result.Count());
+        }
+    }
+}
